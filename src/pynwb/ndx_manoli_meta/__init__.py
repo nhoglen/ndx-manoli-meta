4512e328edc155390b3021db505aadcdbb16fada
nhoglen <13972140+nhoglen@users.noreply.github.com>
2025-10-20 13:48:11 -0700

    Fix init
    Python __init__ file needed the line to add the partner metadata class to the actual install
    
    NB Fix minimal working example with updates

--- src/pynwb/ndx_manoli_meta/__init__.py
+++ src/pynwb/ndx_manoli_meta/__init__.py
@@ -23,6 +23,7 @@ load_namespaces(str(__spec_path))
 # below or write a custom class and register it using the class decorator
 # `@register_class("TetrodeSeries", "ndx-manoli-meta")`
 AssayMetadata = get_class("AssayMetadata", "ndx-manoli-meta")
+IndividualMetadata = get_class('IndividualMetadata','ndx-manoli-meta')
 
 # Remove these functions from the package
 del load_namespaces, get_class
